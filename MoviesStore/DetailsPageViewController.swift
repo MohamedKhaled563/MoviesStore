03ddfdecabcd150990b6962e86b495fd072f3120
MohamedKhaled563 <mohamedkhlidmkm0@gmail.com>
2021-01-27 21:51:58 +0200

    Adding data presistance

--- MoviesStore/DetailsPageViewController.swift
+++ MoviesStore/DetailsPageViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import CoreData
 
 protocol DetailsPageViewControllerDelegate: class {
-    func detailsPageViewControllerDelegateRequireDetails(_ controller: DetailsPageViewController) -> MovieDetails
+//    func detailsPageViewControllerDelegateRequireDetails(_ controller: DetailsPageViewController) -> MovieDetails
+        func detailsPageViewControllerDelegateRequireDetails(_ controller: DetailsPageViewController) -> NSManagedObject
+
 }
 
 
@@ -21,27 +24,35 @@ class DetailsPageViewController: UIViewController {
     
     weak var delegate: DetailsPageViewControllerDelegate?
     
-    var movieDetails: MovieDetails?
-    
+   // var movieDetails: MovieDetails?
+     var movieDetails: NSManagedObject?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
         print("Scene loaded")
         movieDetails = delegate?.detailsPageViewControllerDelegateRequireDetails(self)
-        print ("title: \(movieDetails?.title)")
+        putDataOnScreen()
+        print ("title: \(movieDetails?.value(forKey: "title") ?? "")")
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK:- Helpers
+    //
+    func putDataOnScreen(){
+//        titleLabel.text?.append(movieDetails?.title ?? "")
+//        // TODO:- Add image download pod and download image in and put it in the view
+//        ratingLabel.text?.append("\(movieDetails?.rating ?? 0)" )
+//        releaseYearLabel.text?.append("\(movieDetails?.releaseYear ?? 0)" )
+//        genreLabel.text?.append(movieDetails?.genre.joined(separator: ", ") ?? "")
+        
+        titleLabel.text?.append("\(movieDetails?.value(forKey: "title" ) ?? "")")
+        // TODO:- Add image download pod and download image in and put it in the view
+        ratingLabel.text?.append("\(movieDetails?.value(forKey: "rating") ?? 0)" )
+        releaseYearLabel.text?.append("\(movieDetails?.value(forKey: "releaseYear") ?? 0)" )
+        genreLabel.text?.append("\(movieDetails?.value(forKey: "genre") ?? "")")
     }
-    */
 
 }
--- MoviesStore/MainPageViewController.swift
+++ MoviesStore/MainPageViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class MainPageViewController: UIViewController {
 
     // MARK:- Properties
     //
     var movies: ResultWrapper?
-    var titles: [String] = [""]
+    var savedMovies: [NSManagedObject]?
     var selectedMovieIndex: Int!
     
     // MARK:- Outlets
@@ -20,52 +21,25 @@ class MainPageViewController: UIViewController {
     @IBOutlet weak var moviesTableView: UITableView!
     
     
-    // MARK:- Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let controller = segue.destination as! DetailsPageViewController
-        controller.delegate = self
-    }
-    
-    
+    // MARK:- Methods
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         
         moviesTableView.delegate = self
         moviesTableView.dataSource = self
         
-        
-        
         let cellNib = UINib(nibName: "MoviesTableViewCell", bundle: .none)
         moviesTableView.register(cellNib, forCellReuseIdentifier: "MovieCellIdentifier")
         
-        // Networking
-        guard let url = URL(string: "https://api.androidhive.info/json/movies.json") else {return}
-        let urlRequest = URLRequest(url: url)
-        let urlSession = URLSession(configuration: .default)
-        let dataTask = urlSession.dataTask(with: urlRequest){data,_,error in
-            guard let data = data else {return}
-            /*do{
-                let data = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as! [Dictionary<String, Any>]
-                self.loadDataAndRefreshTable(with: data)
-            }*/
-            do{
-                self.titles.removeAll()
-                let resultWrapper = try JSONDecoder().decode(ResultWrapper.self, from: data)
-                self.loadDataAndRefreshTable(with: resultWrapper)
-                print ("Data loaded")
-            }
-            catch{
-                print("Downloading error: '\(error.localizedDescription)'")
-            }
-        }
-        dataTask.resume()
+        loadMovies()
     }
     
     
     
-    // Helper methods
+    // MARK:- Helper methods
     //
-    func loadDataAndRefreshTable(with data: ResultWrapper){
+    func refreshTable(){
         /*
         movies.removeAll()
         for movie in data{
@@ -78,28 +52,66 @@ class MainPageViewController: UIViewController {
         DispatchQueue.main.async {
             self.moviesTableView.reloadData()
         }*/
-        
-        for movie in data{
-            titles.append(movie.title)
-        }
         DispatchQueue.main.async {
             self.moviesTableView.reloadData()
         }
+        print("Table refreshed")
     }
     
-
+    func loadMovies(){
+        
+        // Trying load data from core data
+        loadDataFromCoreData()
+        
+        // If data exist --> finish loading data
+        if savedMovies?.count != 0 {
+            print("Data is loaded from core data")
+            refreshTable()
+            return
+        }
+        
+        // If data doesn't exist, try download them
+        loadDataFromInternetAndSaveIt()
+        self.moviesTableView.reloadData()
+    }
+    // MARK:- Networking
+    //
+    func loadDataFromInternetAndSaveIt(){
+        guard let url = URL(string: "https://api.androidhive.info/json/movies.json") else {return}
+        let urlRequest = URLRequest(url: url)
+        let urlSession = URLSession(configuration: .default)
+        
+        let dataTask = urlSession.dataTask(with: urlRequest){data,_,error in
+            guard let data = data else {return}
+            /*do{
+                let data = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as! [Dictionary<String, Any>]
+                self.loadDataAndRefreshTable(with: data)
+            }*/
+            do{
+                let resultWrapper = try JSONDecoder().decode(ResultWrapper.self, from: data)
+                self.movies = resultWrapper
+                print ("Data loaded from internet")
+                self.saveData(data: resultWrapper)
+            }
+            catch{
+                print("Downloading error: '\(error.localizedDescription)'")
+            }
+        }
+        dataTask.resume()
+    }
 }
 
-// Table view deletages
+
+// MARK:- Table view deletages
 //
 extension MainPageViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        titles.count
+        savedMovies?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCellIdentifier", for: indexPath) as! MoviesTableViewCell
-        cell.movieNameLabel.text = titles[indexPath.row]
+        cell.movieNameLabel.text = savedMovies?[indexPath.row].value(forKey: "title") as? String
         return cell
     }
 
@@ -107,16 +119,66 @@ extension MainPageViewController: UITableViewDelegate, UITableViewDataSource{
         selectedMovieIndex = indexPath.row
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         let detailsVC = storyBoard.instantiateViewController(identifier: "DetailsVC") as! DetailsPageViewController
+        detailsVC.delegate = self
         navigationController?.pushViewController(detailsVC, animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
-
+// MARK:- Details page delegate methods
+//
 extension MainPageViewController: DetailsPageViewControllerDelegate{
-    func detailsPageViewControllerDelegateRequireDetails(_ controller: DetailsPageViewController) -> MovieDetails {
-        let dummyMovie = MovieDetails(title: "vvv", image: "njkh", rating: 5.6, releaseYear: 23, genre: [""])
-        return dummyMovie
-        //return movies![selectedMovieIndex]
+    func detailsPageViewControllerDelegateRequireDetails(_ controller: DetailsPageViewController) -> NSManagedObject {
+        return savedMovies?[selectedMovieIndex] ?? NSManagedObject()
+    }
+    
+//    func detailsPageViewControllerDelegateRequireDetails(_ controller: DetailsPageViewController) -> MovieDetails {
+//        return movies![selectedMovieIndex]
+//    }
+}
+
+// MARK:- Core Data methods
+//
+extension MainPageViewController{
+    func saveData(data: ResultWrapper){
+        // App delegate
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        // Context
+        let context = appDelegate.persistentContainer.viewContext
+        // NSManagedObject
+        let entity = NSEntityDescription.entity(forEntityName: "Movie", in: context)
+        guard let unwrappedEntity = entity else{return}
+        // Set movie
+        for i in 0..<data.count{
+            let movie = NSManagedObject(entity: unwrappedEntity, insertInto: context)
+            movie.setValue(data[i].title, forKey: "title")
+            movie.setValue(data[i].image, forKey: "image")
+            movie.setValue(data[i].rating, forKey: "rating")
+            movie.setValue(data[i].releaseYear, forKey: "releaseYear")
+            let genreString = data[i].genre.joined(separator: ", ")
+            movie.setValue(genreString, forKey: "genre")
+            do{
+                try context.save()
+                self.savedMovies?.append(movie)
+            } catch{
+                print ("Saving error: '\(error.localizedDescription)'")
+            }
+        }
+        print("Data saved to core data")
+    }
+    
+    func loadDataFromCoreData(){
+        // App delegate
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        // Context
+        let context = appDelegate.persistentContainer.viewContext
+        // Fetching
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Movie")
+        do{
+            savedMovies?.removeAll()
+            try savedMovies = context.fetch(fetchRequest)
+        } catch{
+            print("Cannot fetch the data: '\(error.localizedDescription)'")
+        }
     }
 }
